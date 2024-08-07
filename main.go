3bff52a32af1eeb0bd18538c629df300b7719f63
marasonic <>
2024-08-07 09:42:45 +0200

    new recipe endpoint

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
+
+var recipes []Recipe
 
 type Recipe struct {
 	ID           string    `json:"id"`
@@ -15,7 +19,26 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+// NewRecipeHandler creates a new recipe
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+func init() {
+	recipes = make([]Recipe, 0)
+}
+
 func main() {
 	r := gin.Default()
+	r.POST("/recipes", NewRecipeHandler)
 	r.Run() // listen and serve on 8080
 }
